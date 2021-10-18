7e7c7237279c9275340b05829bd9c46328c2c2ac
MattHill701 <85851376+MattHill701@users.noreply.github.com>
2021-10-17 22:31:06 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// inside index.js
+
+const PORT = 3000;
+const express = require('express');
+const server = express();
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.json())
+
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
+
+  server.get('/background/:color', (req, res, next) => {
+    res.send(`
+      <body style="background: ${ req.params.color };">
+        <h1>Hello World</h1>
+      </body>
+    `);
+  });
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+const { client } = require('./db');
+client.connect();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
